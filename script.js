6d3ec9db13e6b44c192b42b70ac53aa7b75215d8
mellvitoriax <161894873+mellvitoriax@users.noreply.github.com>
2024-09-03 22:49:01 -0300

    03/09

--- script.js
+++ script.js
@@ -51,19 +51,46 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if(atual >=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaPerguntas.textContent = " ";
+    caixaPerguntas.textContent = " ";
     mostraAlternativas();
 }
 
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada (alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++;
+    mostraPergunta ();
+}
+
+
+function mostraResulatdo(){
+    caixaPerguntas.textContent = "Nos dias atuais...";
+    caixaResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent= 
+}
+
+
+
+
+
 
 mostraPergunta();
